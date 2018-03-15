24eddf83120708246d0573a572cd0cccfde78fc3
Rashika Raju <civilrashi24@gmail.com>
2018-03-15 12:35:31 +0530

    Echo added

---
+++ Echo.php
@@ -0,0 +1,18 @@
+<?php
+	//printing string
+	echo "I am a Php Developer";
+	echo "<br>";
+
+	//printing multi line string
+	echo "I 
+	am 
+	a 
+	Php 
+	Developer";
+	echo "<br>";
+
+	echo "I", " am", " a", " Php", " Developer <br>";
+	
+	//printing escape characters
+	echo "Hey \"Php\" Developer ";
+?>	
