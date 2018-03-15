49e319da898200699c717c62096fda4daa3d1d6d
Rashika Raju <civilrashi24@gmail.com>
2018-03-15 18:06:12 +0530

    Print added

---
+++ Print.php
@@ -0,0 +1,20 @@
+<?php
+
+	$name = "Rashika Raju";
+	$age = 22;
+
+	//printing strings
+	print "I am a Thala fan<br>";
+
+	//printing multi line string
+	print "hello
+	php
+	Developer<br>";
+
+	//printing escaping characters
+	print nl2br("hey \"PHP\" Developers\n");
+
+    #printing variable values
+	print "hello I am $name<br>";
+	print "I am ".$age." years old";
+?>	
